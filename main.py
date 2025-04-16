927f9c30d68ba3556410210e9325eed6f212f9e2
Jayo26123 <virgil.teos@gmail.com>
2025-04-16 09:45:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ async def daily_event_post():
     target_time = now.replace(hour=10, minute=0, second=0, microsecond=0)
     if target_time <= now < target_time + timedelta(minutes=1):
         print("Running daily_event_post task...")
-        channel = bot.get_channel(1043088073736585216)
+        channel = bot.get_channel(1130645960113000498)
         if not channel:
             print("Channel not found!")
             return
